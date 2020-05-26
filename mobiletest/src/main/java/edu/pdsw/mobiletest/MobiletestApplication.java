607226f78c3230ee7fc24495dcbd302445bae461
Jacek <rysiuziom1@gmail.com>
2020-05-26 14:43:50 +0200

    initial spring boot project

---
+++ mobiletest/src/main/java/edu/pdsw/mobiletest/MobiletestApplication.java
@@ -0,0 +1,13 @@
+package edu.pdsw.mobiletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiletestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiletestApplication.class, args);
+	}
+
+}
