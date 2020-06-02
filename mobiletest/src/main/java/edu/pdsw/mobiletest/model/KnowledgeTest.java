b4ccf66becb5ddbae6ff4d28b47c4e04ab595161
Jacek Wychowałek <rysiuziom1@gmail.com>
2020-06-02 21:45:18 +0200

    add logger to KnowledgeTest class

--- mobiletest/src/main/java/edu/pdsw/mobiletest/model/KnowledgeTest.java
+++ mobiletest/src/main/java/edu/pdsw/mobiletest/model/KnowledgeTest.java
@@ -1,13 +1,13 @@
 package edu.pdsw.mobiletest.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class KnowledgeTest {
     private final int totalTestTime;
@@ -15,25 +15,26 @@ public class KnowledgeTest {
     private final String exercisesAbsolutePath;
     private List<Exercise> exercises;
 
-    public KnowledgeTest(@JsonProperty("totalTestTime") int totalTestTime, 
-                         @JsonProperty("solutionsAbsolutePath") String solutionsAbsolutePath, 
+    private Logger logger = LoggerFactory.getLogger(KnowledgeTest.class);
+
+    public KnowledgeTest(@JsonProperty("totalTestTime") int totalTestTime,
+                         @JsonProperty("solutionsAbsolutePath") String solutionsAbsolutePath,
                          @JsonProperty("exercisesAbsolutePath") String exercisesAbsolutePath) {
         this.totalTestTime = totalTestTime;
         this.solutionsAbsolutePath = solutionsAbsolutePath;
         this.exercisesAbsolutePath = exercisesAbsolutePath;
         createExerciseList();
     }
-    
+
     private void createExerciseList() {
         File directory = new File(this.exercisesAbsolutePath);
-        System.out.println(directory.getAbsolutePath());
         if (!directory.isDirectory()) {
-            System.out.println(directory.getAbsolutePath() + " is not directory");
+           logger.warn(directory.getAbsolutePath() + " is not directory");
             return;
         }
         var arrayOfFiles = directory.listFiles();
-        if(arrayOfFiles == null) {
-            System.out.println("Empty directory");
+        if (arrayOfFiles == null) {
+            logger.warn(directory.getAbsolutePath() + " is empty directory");
             return;
         }
         exercises = new ArrayList<>();
@@ -43,6 +44,7 @@ public class KnowledgeTest {
                 exercises.add(new Exercise(ex.getAbsolutePath()));
             }
         });
+        logger.info("Exercises directory path: " + directory.getAbsolutePath());
     }
 
     public int getTotalTestTime() {
