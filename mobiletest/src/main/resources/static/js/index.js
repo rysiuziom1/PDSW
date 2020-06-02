ecd657c68bfb43f41806d302bbc71431b7be22b1
patryk-le <58395545+patryk-le@users.noreply.github.com>
2020-06-02 18:38:31 +0200

    Create Test App

---
+++ mobiletest/src/main/resources/static/js/index.js
@@ -0,0 +1,27 @@
+document.getElementById('testForm').addEventListener('submit', check);
+
+function check(event) {
+    event.preventDefault();
+    const url = '/student';
+    const urlEndPoint = 'http://localhost:8080/api/v1/teacher/test/set'
+    const KnowledgeTestData = {
+        "totalTestTime" : document.getElementById('startTime').value,
+        "solutionsAbsolutePath" : document.getElementById('solutionsFolder').value,
+        "exercisesAbsolutePath" : document.getElementById('taskFolder').value,
+    };
+
+    const params = {
+        headers : { "content-type" : "application/json; charset=UTF-8" },
+        body : JSON.stringify(KnowledgeTestData),
+        method : "POST",
+        mode : "cors"
+    };
+    console.log(KnowledgeTestData);
+    fetch(urlEndPoint, params).then(function (response) {
+        if (response.ok) {
+            location.replace(url);
+            console.log("git");
+        }else{
+        document.getElementById("error").innerHTML="Folder nie istnieje!"}
+    })
+}
