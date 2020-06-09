fd3546cf04b08bd21a1ce4c96eb9a671f00b3fd1
Luvkitri <janjedrzejewski@gmail.com>
2020-06-09 21:27:01 +0200

    Added handling parameters for StudentTestController.

--- mobiletest/src/main/java/edu/pdsw/mobiletest/controller/StudentTestController.java
+++ mobiletest/src/main/java/edu/pdsw/mobiletest/controller/StudentTestController.java
@@ -2,11 +2,14 @@ package edu.pdsw.mobiletest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class StudentTestController {
     @GetMapping("/login/student")
-    public String test() {
+    public String test(@RequestParam Map<String, String> parameters) {
         return "student";
     }
 }
