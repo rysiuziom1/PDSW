43bed1741a8199151595451907903cd98e10c5be
Luvkitri <janjedrzejewski@gmail.com>
2020-06-22 19:55:19 +0200

    changed from session storage to local storage

--- mobiletest/src/main/resources/static/js/login.js
+++ mobiletest/src/main/resources/static/js/login.js
@@ -27,7 +27,7 @@ function check(event) {
     fetch(urlEndPoint, params).then(function (response) {
         if (response.ok) {
             console.log("Response from API with code 200");
-            sessionStorage.setItem("studentIndex", studentData.studentIndex);
+            localStorage.setItem("studentIndex", studentData.studentIndex);
             console.log(response.text().then(value => {
                 window.location.href = value;
             }))
