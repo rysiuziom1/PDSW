939dbdc2c5626fea627f46a7ff8d45680e43944b
Luvkitri <janjedrzejewski@gmail.com>
2020-06-22 19:52:33 +0200

    Added client test file download functionality. Now client after login in automatically downloads a file, and also he can use button to download it any time he wants to.

--- mobiletest/src/main/resources/static/js/student.js
+++ mobiletest/src/main/resources/static/js/student.js
@@ -12,16 +12,19 @@ $(document).ready(function () {
             localStorage.setItem("studentUUID", data.studentID);
         });
 
+    getTestFile();
 });
 
 function finishTest() {
     const urlEndPoint = 'http://localhost:8080/api/v1/student/finish_test';
+
     const params = {
         headers : { "content-type" : "application/json" },
         body : JSON.stringify(localStorage.getItem("studentUUID")),
         method : "POST",
         mode : "cors",
     };
+
     fetch(urlEndPoint, params)
         .then(response => {
             if (response.ok) {
@@ -31,8 +34,26 @@ function finishTest() {
         })
 }
 
-function requestTime() {
+function getTestFile() {
+    const urlEndPoint = 'http://localhost:8080/api/v1/student/get_file'
 
+    fetch(urlEndPoint).then(response => {
+        if (response.ok) {
+            response.arrayBuffer().then(function (buffer) {
+                savePdfFile('test-tasks.pdf', buffer);
+            });
+        }
+    })
+}
+
+function savePdfFile(fileName, buffer) {
+    // Object that represents data, in this case pdf
+    let blob = new Blob([buffer], {type: "application/pdf"});
+    // Creates link for this object download in html document
+    let link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = fileName;
+    link.click();
 }
 
 function sendFile() {
@@ -43,7 +64,6 @@ function sendFile() {
 
     formData.append('file', input.files[0]);
 
-    $("#fileList").append('<li class="list-group-item">' + input.files[0].name + '</li>')
 
     const params = {
         body : formData,
@@ -55,9 +75,18 @@ function sendFile() {
         .then(response => {
             if (response.ok) {
                 console.log("File sent to server.")
+                let currentDate = new Date();
+                let datetime = "added: " + currentDate.getDate() + "/"
+                    + (currentDate.getMonth()+1)  + "/"
+                    + currentDate.getFullYear() + " at "
+                    + currentDate.getHours() + ":"
+                    + currentDate.getMinutes() + ":"
+                    + currentDate.getSeconds();
+
+                $("#fileList").append('<li class="list-group-item">' + input.files[0].name + '  ' + datetime + '</li>')
             }
-        }).then(
-            success => console.log(success) // Handle the success response object
+        }).then(success => console.log(success)
+
         ).catch(
             error => console.log(error) // Handle the error response object
         );
